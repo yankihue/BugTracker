ed1b5e64117eb06c66d766f9497c60253f61d023
yankihue <yankisirtas@hotmail.com>
2021-04-12 17:02:42 +0300

    Add react with babel

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bugs.apps.BugsConfig'
+    'bugs.apps.BugsConfig',
+    'frontend',
 ]
 
 MIDDLEWARE = [
