0a941bea2885f9cbf9569f96d58a5567eed493a1
yankihue <yankisirtas@hotmail.com>
2021-04-14 00:05:46 +0300

    Remove unused imports

--- bugs/views.py
+++ bugs/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Bug, Comment
-from django.template import loader
 from rest_framework import viewsets
 from .serializers import BugSerializer, CommentSerializer
 
